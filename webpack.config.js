72e04ecd379e314270b26f41ae7e66e99631cb33
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-05-26 12:26:28 +0300

    added minification of js file for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,39 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
-    entry: [
-        'babel-polyfill',
-        './lib/renderers/dom.js',
-    ],
+    resolve: {
+        modules: [
+            path.resolve('./lib'),
+            path.resolve('./node_modules')
+        ]
+    },
+    entry: {
+        vendor: [
+            'babel-polyfill',
+            'react',
+            'react-dom',
+            'prop-types',
+            'axios',
+            'lodash.debounce',
+            'lodash.pickby'
+        ],
+        app: ['./lib/renderers/dom.js']
+    },
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
         rules: [
             { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+    ]
 };
 
 module.exports = config;
